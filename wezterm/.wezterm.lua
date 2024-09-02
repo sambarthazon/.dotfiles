c29cc2689d3cfdcd2876deb933ca5fe7f270e054
sambarthazon <sam.barthazon@gmail.com>
2024-09-02 13:36:02 +0300

    Upload my conf on GitHub
    Commit done with Lazygit to test the tool.

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 18
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 10
+
+-- To display Wezterm in the center of the screen
+wezterm.on("gui-startup", function(cmd)
+	local screen = wezterm.gui.screens().main
+	local ratio = 0.7
+	local width, height = screen.width * ratio, screen.height * ratio
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or {
+		position = { x = (screen.width - width) / 2, y = (screen.height - height) / 2 },
+	})
+	-- window:gui_window():maximize()
+	window:gui_window():set_inner_size(width, height)
+end)
+
+-- and finally, return the configuration to wezterm
+return config
