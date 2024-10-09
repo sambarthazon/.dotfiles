a3f7cafe93241c6f1ddf1f68bd3dc74fcaf13f12
sambarthazon <sam.barthazon@gmail.com>
2024-10-09 21:45:33 +0300

    Added compile and generate features

--- tex/mytex.sh
+++ tex/mytex.sh
@@ -4,11 +4,13 @@ template_path=~/.dotfiles/tex/Template/
 template_file=template.tex
 
 show_help() {
-  echo "Usage: ./$(basename $0) [-h] [-f argument] [-p argument]"
+  echo "Usage: ./$(basename $0) [-h] [-f filename] [-p path] [-c filename] [-g filename]"
   echo "Options:"
   echo "  -h            Display this message"
   echo "  -f            Create new tex file based on template.tex"
   echo "  -p            Create new tex project based on Template project"
+  echo "  -c            Compile the latex file"
+  echo "  -g            Generate pdf file for the latex file"
   echo ""
   echo "This script create for you a template for a file or a project."
 
@@ -25,6 +27,16 @@ create_project() {
   return 0
 }
 
+compile_file() {
+  latex ${1} && echo "Latex file compiled."
+  return 0
+}
+
+generate_file() {
+  pdflatex ${1} && echo "PDF generated."
+  return 0
+}
+
 while getopts ':hf:p:' OPTION; do
   case "$OPTION" in
     h)
@@ -35,6 +47,12 @@ while getopts ':hf:p:' OPTION; do
     p)
       arg_p="$OPTARG"
       create_project ${arg_p};;
+    c)
+      arg_c="$OPTARG"
+      compile_file ${arg_c};;
+    g)
+      arg_g="$OPTARG"
+      generate_file ${arg_g};;
     ??)
       show_help;;
   esac
